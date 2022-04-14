b78848a147fb51476f92eca2b998366546d4f22e
Ivan <ivan4ik.ru70@gmail.com>
2022-04-14 17:12:21 +0700

    добавил реализацию 2 и 3 пунтов

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 10_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -21,11 +21,21 @@ public class Main {
                 .count();
         System.out.println("1. Количество несовершеннолетних: " + count);
 
-        persons.stream()
-                .filter(age -> age.getAge() >= 18 && age.getAge() < 27)
-                .map(String::valueOf)
-//                .map(famili -> families)
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+        List<String> recruits = persons.stream()
+                .filter(f -> f.getAge() >= 18 && f.getAge() < 27 && f.getSex() == Sex.MAN)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println("2. Фамилии призывников: ");
+//        recruits.forEach(System.out::println);
+
+        List<String> jobs = persons.stream()
+                .filter((f) -> (f.getAge() >= 18 && f.getEducation() == Education.HIGHER))
+                .filter((f) -> (f.getSex() == Sex.WOMAN && f.getAge() < 55) || (f.getSex() == Sex.MAN && f.getAge() < 60))
+//                .map()
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+//        System.out.println("3. Фамилии работящих: ");
+                jobs.forEach(System.out::println);
     }
 }
+//
